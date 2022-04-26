71465cec5d5ae1cf7479ab112f06deb16548b404
Fenyep <tenordubois10@gmail.com>
2022-04-26 15:10:12 +0100

    ADD : addition of the init() method to the TeacherModel file

---
+++ server/models/TeacherModel.js
@@ -0,0 +1,27 @@
+import connection from "../utils/index.js";
+
+class TeacherModel {
+    
+
+    /**
+     * 
+     */
+    static async init () {
+      const query = `
+        CREATE TABLE IF NOT EXISTS Enseignant
+        (
+          matriculeEns VARCHAR(255) NOT NULL PRIMARY KEY,
+          nomEns VARCHAR(255) NOT NULL,
+          sexEns VARCHAR(255) NOT NULL
+        )
+      `
+
+      try {
+        await connection.query(query)
+  
+        console.log("Table Enseignant OK")
+      } catch (err) {
+        console.log(err)
+      }
+    }
+}
