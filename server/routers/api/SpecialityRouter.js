ec69054329db84c2919e96690d2d9612fd5fca83
Fenyep <tenordubois10@gmail.com>
2022-04-27 00:07:15 +0100

    ADD : addition of the SpecialityRouter file

--- server/routers/api/SpecialityRouter.js
+++ server/routers/api/SpecialityRouter.js
@@ -3,6 +3,6 @@ import SpecialityController from "../../controllers/SpecialityController.js"
 
 const SpecialityRouter = express.Router()
 
-SpecialityRouter.post('/create', SpecialityController.createSpeciality)
+// SpecialityRouter.post('/create', SpecialityController.createSpeciality)
 
 export default SpecialityRouter
