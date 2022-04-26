88a0fba354c6b321a8bc8487a6f26ff183527bd4
Fenyep <tenordubois10@gmail.com>
2022-04-27 00:14:24 +0100

    ADD : addition of the TeacherContoller file

---
+++ server/controllers/TeacherController.js
@@ -0,0 +1,20 @@
+import TeacherModel from "../models/TeacherModel.js"
+
+class TeacherController {
+
+    /**
+     * 
+     */
+    static getSpeciality = async (req, res) => {
+        res.send("This are all Teachers")
+    }
+
+    /**
+     * 
+     */
+    static createSpeciality = async (req, res) => {
+
+    }
+}
+
+export default TeacherController
