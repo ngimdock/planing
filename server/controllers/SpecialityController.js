3e88284839ef87c9d2abe28745bb479ac8f18205
Fenyep <tenordubois10@gmail.com>
2022-04-27 00:12:32 +0100

    ADD : addition of the SpecialityContoller file

---
+++ server/controllers/SpecialityController.js
@@ -0,0 +1,20 @@
+import SpecialityModel from "../models/SpecialityModel.js"
+
+class SpecialityController {
+
+    /**
+     * 
+     */
+    static getSpeciality = async (req, res) => {
+        res.send("This are all Specialities")
+    }
+
+    /**
+     * 
+     */
+    static createSpeciality = async (req, res) => {
+
+    }
+}
+
+export default SpecialityController
