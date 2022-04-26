02fc5747603d09d398affa8d7cb623aca7e596a6
Fenyep <tenordubois10@gmail.com>
2022-04-27 00:24:13 +0100

    ADD : addition of the GroupRouter file

---
+++ server/routers/api/GroupRouter.js
@@ -0,0 +1,8 @@
+import express from "express";
+import GroupController from "../../controllers/GroupController.js"
+
+const GroupRouter = express.Router()
+
+// GroupRouter.post('/create', GroupController.createGroup)
+
+export default GroupRouter
