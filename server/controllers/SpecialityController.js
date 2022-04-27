893015f1868633f65296b911898843ba9c9f9be2
Fenyep <tenordubois10@gmail.com>
2022-04-27 09:40:19 +0100

    ADD : Implementation of the createSpeciality() method in the SpecialityController file

--- server/controllers/SpecialityController.js
+++ server/controllers/SpecialityController.js
@@ -11,9 +11,26 @@ class SpecialityController {
 
     /**
      * 
+     * @param {String} nomSpecialite The name of the Speciality
+     * @returns {Object} data | error
      */
     static createSpeciality = async (req, res) => {
+        const {
+            nomSpecialite
+        } = req.body
 
+
+        if(nomSpecialite) {
+
+            const data = await SpecialityModel.create(data)
+
+            if(data) 
+                return res.status(201).json(data)
+                
+            return res.status(500).json({ error: "An error occured" })
+        } else {
+            return res.status(400).json({ error: "Provide all the required data" })
+        }
     }
 }
 
