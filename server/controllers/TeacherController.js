e26a4159bb83d0f95f7924741d2f301d1971ae63
Fenyep <tenordubois10@gmail.com>
2022-04-27 08:06:26 +0100

    DELETE : Removal of the GroupController file
    FEATURE : implementation of the createTeacher() method in the TeacherController file

--- server/controllers/TeacherController.js
+++ server/controllers/TeacherController.js
@@ -13,6 +13,23 @@ class TeacherController {
      * 
      */
     static createTeacher = async (req, res) => {
+        // Get data from the request body
+        const {
+            matriculeEns,
+            nomEns,
+            sexEns
+        } = req.body
+
+        if (matriculeEns && nomEns && sexEns) {
+            const { data } = await TeacherModel.create(req.body)
+
+            if(data)
+                return res.status(201).json(data)
+
+            return res.status(500).json({ error: "an error occured" })
+        } else {
+            return res.status(400).json({ error: "Provide all the required data" })
+        }
 
     }
 }
