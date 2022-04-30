9e5c77384826e93eec42abbc7faf807ae1987224
Fenyep <tenordubois10@gmail.com>
2022-04-30 21:46:46 +0100

    ADD: addition of the search by *name* router in the SpecialityRouter file

--- server/routers/api/SpecialityRouter.js
+++ server/routers/api/SpecialityRouter.js
@@ -4,6 +4,7 @@ import SpecialityController from "../../controllers/SpecialityController.js"
 const SpecialityRouter = express.Router()
 
 SpecialityRouter.get('/', SpecialityController.getSpecialities)
+SpecialityRouter.get('/:nomSpecialite', SpecialityController.getSpeciality)
 SpecialityRouter.post('/create', SpecialityController.createSpeciality)
 
 export default SpecialityRouter
