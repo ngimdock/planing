f9ad3367885f9504c6d6269af1c98ee488c7c2cd
Fenyep <tenordubois10@gmail.com>
2022-04-30 08:29:11 +0100

    ADD : adding of the get teacher by matricule route in the TeacherRouter file

--- server/routers/api/TeacherRouter.js
+++ server/routers/api/TeacherRouter.js
@@ -4,6 +4,7 @@ import TeacherController from "../../controllers/TeacherController.js"
 const TeacherRouter = express.Router()
 
 TeacherRouter.get("/", TeacherController.getTeachers)
+TeacherRouter.get("/:matriculeEns", TeacherController.getTeacher)
 TeacherRouter.post('/create', TeacherController.createTeacher)
 TeacherRouter.put('/update/:currentMatriculeEns', TeacherController.updateTeacher)
 // TeacherRouter.patch('/delete', TeacherController.updateTeacher)
