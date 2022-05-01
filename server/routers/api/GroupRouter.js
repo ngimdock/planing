7f765d068efeafa27f5da8f5870de818266567ec
Fenyep <tenordubois10@gmail.com>
2022-05-01 09:09:43 +0100

    ADD: addition of the fetching all groups router in the GroupRouter file

--- server/routers/api/GroupRouter.js
+++ server/routers/api/GroupRouter.js
@@ -3,6 +3,7 @@ import GroupController from "../../controllers/GroupController.js"
 
 const GroupRouter = express.Router() 
 
+GroupRouter.get('/', GroupController.getGroups)
 GroupRouter.post('/create', GroupController.createGroup)
 
 export default GroupRouter
