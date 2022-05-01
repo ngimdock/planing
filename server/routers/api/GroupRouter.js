ce2a2fa01010d44eca0831ff1895befcc66bc08f
Fenyep <tenordubois10@gmail.com>
2022-05-01 09:52:40 +0100

    ADD: addition of the delete one specific group infos router in the GroupRouter file

--- server/routers/api/GroupRouter.js
+++ server/routers/api/GroupRouter.js
@@ -7,5 +7,6 @@ GroupRouter.get('/', GroupController.getGroups)
 GroupRouter.get('/:idGroupe', GroupController.getGroup)
 GroupRouter.post('/create', GroupController.createGroup)
 GroupRouter.put('/update/:idGroupe', GroupController.updateGroup)
+GroupRouter.delete('/delete/:idGroupe', GroupController.deleteGroup)
 
 export default GroupRouter
