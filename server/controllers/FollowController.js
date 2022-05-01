28fb52944f47ca92880cf60ca31067c815286245
Fenyep <tenordubois10@gmail.com>
2022-05-01 18:52:01 +0100

    ADD: addition and implentation of the createFollow() method in the FollowController file

--- server/controllers/FollowController.js
+++ server/controllers/FollowController.js
@@ -10,9 +10,32 @@ class FollowController {
     }
 
     /**
-     * 
+     * Fetching the new assocation of a group and a course
+     * @param {number} idGroupe the reference of the Groupe table
+     * @param {String} codeCours the reference of the Cours table
+     * @returns {Object} data | error
      */
     static createFollow = async (req, res) => {
+        const { 
+            idGroupe,
+            codeCours
+        } = req.body 
+
+        if(idGroupe && codeCours 
+            && Number.isInteger(idGroupe) 
+            && typeof codeCours === 'string' && codeCours != '') {
+                const data = await FollowModel.create(req.body)
+
+                if(data) {
+                    res.status(200).json(data)
+                } else {
+                    res.status(500).json({ error: " An error occured " })
+                }
+                
+        } else {
+            res.status(400).json({ error : " Provide all required infos "})
+        }
+
 
     }
 }
