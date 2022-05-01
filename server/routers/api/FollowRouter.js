fde5360880341f870de3fbe2a8c98eb2c1973b17
Fenyep <tenordubois10@gmail.com>
2022-05-01 22:29:52 +0100

    ADD: addtion of the delete route in the FollowRouter file

--- server/routers/api/FollowRouter.js
+++ server/routers/api/FollowRouter.js
@@ -5,5 +5,5 @@ const FollowRouter = express.Router()
 
 FollowRouter.post('/create', FollowController.createFollow)
 FollowRouter.put('/update/:currentIdGroupe&:currentCodeCours', FollowController.updateFollow)
-
+FollowRouter.delete('/delete/:idGroupe&:codeCours', FollowController.deleteFollow)
 export default FollowRouter
