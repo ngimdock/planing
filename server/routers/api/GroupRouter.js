5f028fd01d6c36d5776792d1477afaee91b871ad
Fenyep <tenordubois10@gmail.com>
2022-05-01 09:20:22 +0100

    ADD: addition of the fetching of one specific group router in the GroupRouter file

--- server/routers/api/GroupRouter.js
+++ server/routers/api/GroupRouter.js
@@ -4,6 +4,7 @@ import GroupController from "../../controllers/GroupController.js"
 const GroupRouter = express.Router() 
 
 GroupRouter.get('/', GroupController.getGroups)
+GroupRouter.get('/:idGroupe', GroupController.getGroup)
 GroupRouter.post('/create', GroupController.createGroup)
 
 export default GroupRouter
