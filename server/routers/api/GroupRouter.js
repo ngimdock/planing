2018831f3bd1ed545759dead042d3c27fe53918b
Fenyep <tenordubois10@gmail.com>
2022-05-01 09:43:01 +0100

    ADD: addition of the update one specific group infos router in the GroupRouter file

--- server/routers/api/GroupRouter.js
+++ server/routers/api/GroupRouter.js
@@ -6,5 +6,6 @@ const GroupRouter = express.Router()
 GroupRouter.get('/', GroupController.getGroups)
 GroupRouter.get('/:idGroupe', GroupController.getGroup)
 GroupRouter.post('/create', GroupController.createGroup)
+GroupRouter.put('/update/:idGroupe', GroupController.updateGroup)
 
 export default GroupRouter
