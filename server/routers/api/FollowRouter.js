6bc42a3695a44bd5ced9c27e246d4d587686080f
Fenyep <tenordubois10@gmail.com>
2022-05-01 18:49:58 +0100

    ADD: addition of the creation router in the FolloWRouter file

--- server/routers/api/FollowRouter.js
+++ server/routers/api/FollowRouter.js
@@ -3,6 +3,6 @@ import FollowController from "../../controllers/FollowController.js"
 
 const FollowRouter = express.Router()
 
-// FollowRouter.post('/create', FollowController.createFollow)
+FollowRouter.post('/create', FollowController.createFollow)
 
 export default FollowRouter
