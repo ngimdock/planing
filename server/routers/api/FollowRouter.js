dc2c5dbb312590d8c0d3ccb31fe968a3f66bb244
Fenyep <tenordubois10@gmail.com>
2022-05-02 05:50:54 +0100

    ADD: addition of the *groups by course* route in the FollowRouter file

--- server/routers/api/FollowRouter.js
+++ server/routers/api/FollowRouter.js
@@ -3,6 +3,7 @@ import FollowController from "../../controllers/FollowController.js"
 
 const FollowRouter = express.Router()
 
+FollowRouter.get('/groups/:codeCours', FollowController.getGroupsByCourse)
 FollowRouter.post('/create', FollowController.createFollow)
 FollowRouter.put('/update/:currentIdGroupe&:currentCodeCours', FollowController.updateFollow)
 FollowRouter.delete('/delete/:idGroupe&:codeCours', FollowController.deleteFollow)
