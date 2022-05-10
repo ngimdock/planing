65c775a6229c4e579b397feee439dea46366e7ae
Fenyep <tenordubois10@gmail.com>
2022-05-10 15:52:25 +0100

    FEATURE: Possibility to update teachers infos in the client side of the platform

--- server/controllers/TeacherController.js
+++ server/controllers/TeacherController.js
@@ -94,7 +94,7 @@ class TeacherController {
                 const response = await TeacherModel.getById(matriculeEns)
 
                 if(response.length > 0){
-                    res.status(200).json(response)    
+                    res.status(200).json(...response)    
                 } else {
                     res.status(404).json({ error: "No such teacher "})
                 }  
