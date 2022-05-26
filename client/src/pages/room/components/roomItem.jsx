b0deaa0b1ab308d1d0dbe3bf47da3f2e000dec11
Fenyep <tenordubois10@gmail.com>
2022-05-26 15:19:34 +0100

    ADD: addition and semi-implementation of the program export by room logic

--- client/src/pages/room/components/roomItem.jsx
+++ client/src/pages/room/components/roomItem.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FiEdit2} from 'react-icons/fi'
 import {RiDeleteBin6Line} from 'react-icons/ri'
 import styles from "../css/roomStyle.module.css"
+import { MdOutlineFileUpload } from 'react-icons/md'
+import {Box} from '@mui/material';
+import { ExportContext } from '../../../datamanager/contexts/exportContext'
+import ExportBaseLayout from '../../exports/base'
 
 const RoomItem = ({ title, value, color }) => {
-   
+  // Get global state
+  const { exportRef, handlePrintByRoom } = useContext(ExportContext)
+
     return (
         <div className={styles.roomItem}
             style={{
@@ -13,6 +19,22 @@ const RoomItem = ({ title, value, color }) => {
             <span className={styles.Description}> { title } </span>
             <span className={styles.number}> { value } </span>
             <div className={styles.levelIcon}>
+              <Box 
+                sx={{
+                  display: "flex",
+                  "&:hover": {
+                    cursor: 'pointer'
+                  }
+                }}
+                onClick={handlePrintByRoom}
+              >
+
+                <div style={{ display: "none" }}><ExportBaseLayout ref={exportRef} /></div>
+                <MdOutlineFileUpload 
+                  color="#3b3e41"
+                  size={23}
+                />
+              </Box>
                 <FiEdit2
                   size="18"
                   color="#3b3e41"
@@ -20,9 +42,8 @@ const RoomItem = ({ title, value, color }) => {
                 < RiDeleteBin6Line
                   size="18"
                   color="#ff0000"
-                
                 />
-              </div>
+            </div>
         </div>
   )
 }
