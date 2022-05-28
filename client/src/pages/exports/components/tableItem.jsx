f1bafed0c62631087557403f47bf289d8cbf39c9
Fenyep <tenordubois10@gmail.com>
2022-05-28 02:10:52 +0100

    ADD: addition and implementation of the handleSetPrograms methods and the logic of filling the table with corresponding infos

--- client/src/pages/exports/components/tableItem.jsx
+++ client/src/pages/exports/components/tableItem.jsx
@@ -1,10 +1,39 @@
-import React, { useContext } from 'react'
-import { ExportContext } from '../../../datamanager/contexts/exportContext'
+import React, { useMemo } from 'react'
 import styles from '../css/exports.module.css'
 
-const TableItem = () => {
+const TableItem = ({ program }) => {
+    // some handlers
+    const handleSetTablePrograms = (program, day) => {
+        if(program) {
+            switch(day) {
+                case "monday":
+                    return program.Lundi
+                case "tuesday":
+                    return program.Mardi
+                case "wednesday":
+                    return program.Mercredi
+                case "thursday":
+                    return program.Jeudi
+                case "friday":
+                    return program.Vendredi
+                case "saturday":
+                    return program.Samedi
+                case "sunday":
+                    return program.Dimanche
+                default:
+                    return null
+            }
+        }
+    }
 
-    const { placeExport } = useContext(ExportContext)
+    // set local state
+    const mondayPrograms = useMemo(() => handleSetTablePrograms(program, "monday"),[program])
+    const tuesdayPrograms = useMemo(() => handleSetTablePrograms(program,  "tuesday"),[program])
+    const wednesdayPrograms = useMemo(() => handleSetTablePrograms(program, "wednesday"),[program])
+    const thursdayPrograms = useMemo(() => handleSetTablePrograms(program, "thursday"),[program])
+    const fridayPrograms = useMemo(() => handleSetTablePrograms(program, "friday"),[program])
+    const saturdayPrograms = useMemo(() => handleSetTablePrograms(program, "saturday"),[program])
+    const sundayPrograms = useMemo(() => handleSetTablePrograms(program, "sunday"),[program])
 
     const headerInfos = [
         "Heures", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"
@@ -30,40 +59,244 @@ const TableItem = () => {
            </thead>
            <tbody>
                 <tr>
-                    <td className={styles.td}>7h - 9h55 {placeExport}</td>
+                    <td className={styles.td}>7h - 9h55</td>
 
-                    {bodyInfos.map((item) => (
-                        <td className={styles.td}>{item}</td>
-
-                    ))}
+                    <td className={styles.td}>{
+                        mondayPrograms ? 
+                        mondayPrograms.map((course) => 
+                            course.startHour / 3600 >= 7 && course.endHour / 3600 <= 10 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        tuesdayPrograms ? 
+                        tuesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 7 && course.endHour / 3600 <= 10 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        wednesdayPrograms ? 
+                        wednesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 7 && course.endHour / 3600 <= 10 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        thursdayPrograms ? 
+                        thursdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 7 && course.endHour / 3600 <= 10 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        fridayPrograms ? 
+                        fridayPrograms.map((course) => 
+                            course.startHour / 3600 >= 7 && course.endHour / 3600 <= 10 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        saturdayPrograms ? 
+                        saturdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 7 && course.endHour / 3600 <= 10 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        sundayPrograms ? 
+                        sundayPrograms.map((course) => 
+                            course.startHour / 3600 >= 7 && course.endHour / 3600 <= 10 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td> 
                 </tr>
                 <tr>
                     <td className={styles.td}>10h05 - 12h55</td>
-                    {bodyInfos.map((item) => (
-                        <td className={styles.td}>{item}</td>
 
-                    ))}
+                    <td className={styles.td}>{
+                        mondayPrograms ? 
+                        mondayPrograms.map((course) => 
+                            course.startHour / 3600 >= 10 && course.endHour / 3600 <= 13 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        tuesdayPrograms ? 
+                        tuesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 10 && course.endHour / 3600 <= 13 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        wednesdayPrograms ? 
+                        wednesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 10 && course.endHour / 3600 <= 13 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        thursdayPrograms ? 
+                        thursdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 10 && course.endHour / 3600 <= 13 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        fridayPrograms ? 
+                        fridayPrograms.map((course) => 
+                            course.startHour / 3600 >= 10 && course.endHour / 3600 <= 13 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        saturdayPrograms ? 
+                        saturdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 10 && course.endHour / 3600 <= 13 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        sundayPrograms ? 
+                        sundayPrograms.map((course) => 
+                            course.startHour / 3600 >= 10 && course.endHour / 3600 <= 13 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
                 </tr>
                 <tr>
                     <td className={styles.td}>13h15 - 15h55</td>
-                    {bodyInfos.map((item) => (
-                        <td className={styles.td}>{item}</td>
 
-                    ))}
+                    <td className={styles.td}>{
+                        mondayPrograms ? 
+                        mondayPrograms.map((course) => 
+                            course.startHour / 3600 >= 13 && course.endHour / 3600 <= 16 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        tuesdayPrograms ? 
+                        tuesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 13 && course.endHour / 3600 <= 16 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        wednesdayPrograms ? 
+                        wednesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 13 && course.endHour / 3600 <= 16 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        thursdayPrograms ? 
+                        thursdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 13 && course.endHour / 3600 <= 16 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        fridayPrograms ? 
+                        fridayPrograms.map((course) => 
+                            course.startHour / 3600 >= 13 && course.endHour / 3600 <= 16 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        saturdayPrograms ? 
+                        saturdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 13 && course.endHour / 3600 <= 16 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        sundayPrograms ? 
+                        sundayPrograms.map((course) => 
+                            course.startHour / 3600 >= 13 && course.endHour / 3600 <= 16 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
                 </tr>
                 <tr>
                     <td className={styles.td}>16h05 - 18h55</td>
-                    {bodyInfos.map((item) => (
-                        <td className={styles.td}>{item}</td>
 
-                    ))}
-                </tr>
-                <tr>
-                    <td className={styles.td}>19h05 - 21h</td>
-                    {bodyInfos.map((item) => (
-                        <td className={styles.td}>{item}</td>
-
-                    ))}
+                    <td className={styles.td}>{
+                        mondayPrograms ? 
+                        mondayPrograms.map((course) => 
+                            course.startHour / 3600 >= 16 && course.endHour / 3600 <= 19 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        tuesdayPrograms ? 
+                        tuesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 16 && course.endHour / 3600 <= 19 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        wednesdayPrograms ? 
+                        wednesdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 16 && course.endHour / 3600 <= 19 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        thursdayPrograms ? 
+                        thursdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 16 && course.endHour / 3600 <= 19 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        fridayPrograms ? 
+                        fridayPrograms.map((course) => 
+                            course.startHour / 3600 >= 16 && course.endHour / 3600 <= 19 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        saturdayPrograms ? 
+                        saturdayPrograms.map((course) => 
+                            course.startHour / 3600 >= 16 && course.endHour / 3600 <= 19 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
+                    <td className={styles.td}>{
+                        sundayPrograms ? 
+                        sundayPrograms.map((course) => 
+                            course.startHour / 3600 >= 16 && course.endHour / 3600 <= 19 ?
+                                (<div> {`${course.subjectCode} (${course.roomName}) ${course.teacherName}`} </div>)
+                            : null
+                        ) : null
+                    }</td>
                 </tr>
            </tbody>
         </table>
