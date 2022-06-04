341041cb469e0fc35c280d736e73c20c5d59a989
Fenyep <tenordubois10@gmail.com>
2022-06-04 01:26:41 +0100

    UPDATE: Amelioration of the multi-structured programs gestions from all the export locations in this file, optimized to a given rendering

--- client/src/pages/exports/base.jsx
+++ client/src/pages/exports/base.jsx
@@ -4,6 +4,7 @@ import ExportFooter from './components/subComponents/exportFooter'
 import styles from './css/exports.module.css'
 import TableContainer from './components/tableContainer'
 import { ExportContext } from '../../datamanager/contexts/exportContext'
+import FacultyPage from './components/subComponents/facultyPage'
 
 const ExportBaseLayout = React.forwardRef((props, ref) => {
   // get program global state
@@ -12,10 +13,24 @@ const ExportBaseLayout = React.forwardRef((props, ref) => {
   return (
     <div ref={ref} className={styles.wrapper}
     >
-      <ExportHeader semestre={programs.semester} anneeAcademique={programs.academicYear} />
-      {/* {programs.map(program => ( */}
-      <TableContainer programs={programs} />
-      {/* ))} */}
+      <ExportHeader semestre={programs.semester} anneeAcademique={programs.academicYear} code={programs.facultyName || programs.code} />
+      { programs.code ? (
+          <TableContainer programs={programs} semestre={programs.semester} anneeAcademique={programs.academicYear}/> 
+        ) : programs.prevResult ? (
+              programs.prevResult.map(faculty => (
+                <div>
+                  <FacultyPage name={faculty.facultyName} />
+                  {faculty.classes.map(program => (
+                    <TableContainer programs={program} semestre={programs.semester} anneeAcademique={programs.academicYear}/>
+                  ))}
+                </div>
+              ))
+            ):(
+              programs.classes && programs.classes.map(program => (
+                <TableContainer programs={program} semestre={programs.semester} anneeAcademique={programs.academicYear}/>
+              ))
+            )
+      }
       <ExportFooter semestre={programs.semester} anneeAcademique={programs.academicYear} />
     </div>
   )
